94b4a60898e10b88a7fd0484450d70a832b38da0
lenovo <lenovo@DESKTOP-4Q1AOBJ>
2019-11-26 20:20:48 +0530

    branchx commit

---
+++ src/com/nt/test/ApplicationTest2.java
@@ -0,0 +1,17 @@
+package com.nt.test;
+
+public class ApplicationTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 String q1="Hellow";
+		if (q1=="Hello") {
+			System.out.println("Hello good night");
+		}
+		else {
+			System.out.println("Hellow good night");
+		}
+
+	}
+
+}
