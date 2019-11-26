cbc9284e4ea842d670a2c864b1eda614ea758e8a
lenovo <lenovo@DESKTOP-4Q1AOBJ>
2019-11-26 19:14:51 +0530

    first commit

---
+++ src/com/nt/test/ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.nt.test;
+
+public class ApplicationTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 String q1="Hellow";
+		if (q1=="Hello") {
+			System.out.println("Hello good morning");
+		}
+		else {
+			System.out.println("Hellow good morning");
+		}
+
+	}
+
+}
